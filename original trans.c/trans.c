d0ab6f424402f8244d471dcd940155947cc988ce
bhavanarajendran2007-boop <bhavanarajendran2007@gmail.com>
2026-05-13 21:22:04 +0530

    Account number validation included

--- original trans.c/trans.c
+++ original trans.c/trans.c
@@ -154,6 +154,11 @@ void updateRecord(FILE *fPtr)
     // obtain number of account to update
     printf("%s", "Enter account to update ( 1 - 100 ): ");
     scanf("%d", &account);
+    if(account < 1 || account > 100)
+{
+    printf("Invalid Account Number!\n");
+    return;
+}
 
     // move file pointer to correct record in file
     fseek(fPtr, (account - 1) * sizeof(struct clientData), SEEK_SET);
@@ -193,6 +198,11 @@ void deleteRecord(FILE *fPtr)
     // obtain number of account to delete
     printf("%s", "Enter account number to delete ( 1 - 100 ): ");
     scanf("%d", &accountNum);
+    if(accountNum < 1 || accountNum > 100)
+{
+    printf("Invalid Account Number!\n");
+    return;
+}
 
     // move file pointer to correct record in file
     fseek(fPtr, (accountNum - 1) * sizeof(struct clientData), SEEK_SET);
@@ -222,6 +232,11 @@ void newRecord(FILE *fPtr)
     // obtain number of account to create
     printf("%s", "Enter new account number ( 1 - 100 ): ");
     scanf("%d", &accountNum);
+    if(accountNum < 1 || accountNum > 100)
+{
+    printf("Invalid Account Number!\n");
+    return;
+}
 
     // move file pointer to correct record in file
     fseek(fPtr, (accountNum - 1) * sizeof(struct clientData), SEEK_SET);
